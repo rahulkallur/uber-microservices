399637c34c04038d9be83fe6ad89975084e749e7
rahulkallur <rahulkallur63@gmail.com>
2025-03-02 19:37:02 +0530

    updated routes

--- user/routes/routes.js
+++ user/routes/routes.js
@@ -1,5 +1,19 @@
 import express from "express";
+import {
+  login,
+  logout,
+  register,
+  profile,
+  acceptedRides,
+} from "../controllers/userController.js";
+import { userAuth } from "../middleware/authMiddleWare.js";
 
 const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", userAuth, logout);
+router.get("/profile", userAuth, profile);
+router.get("/accepted-rides", userAuth, acceptedRides);
 
 export default router;
